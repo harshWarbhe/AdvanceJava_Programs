48b35dc89dae4486fd46564c4292ac8dd7e81a40
harsh <harshwarbhe@gmail.com>
2025-07-16 15:54:55 +0530

    commit 1.11 security MODE- Basic, digest, form, client-cert

---
+++ LinkApp-security-FORM/src/main/java/com/harsh/servlet/LinksServlet.java
@@ -0,0 +1,64 @@
+package com.harsh.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LinksServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// get printWriter
+		PrintWriter pw = resp.getWriter();
+
+		// set context type
+		resp.setContentType("text/html");
+		// read special request param value p1
+		String pval = req.getParameter("p1");
+		
+		//get all available Locales
+		Locale locale[] = Locale.getAvailableLocales();
+		
+		//differentiate logic for hyperlinks
+		if (pval.equalsIgnoreCase("link1")) {
+			pw.println("<h1 style='color: red; text-align: center;'>All language are :");
+			for (Locale l : locale) {
+				pw.println(l.getDisplayLanguage()+"<br>");
+			}
+		}
+		
+		else if (pval.equalsIgnoreCase("link2")) {
+			pw.println("<h1 style='color: red; text-align: center;'>All countries are:");
+			for (Locale l : locale) {
+				if (!l.getDisplayCountry().isEmpty()) 
+			        pw.println(l.getDisplayCountry() + "<br>");
+			}
+
+		}
+		
+		else {
+			pw.println("<h1 style='color: red; text-align: center;'> System Date and time : "+LocalDateTime.now()+"</h1>");
+		}
+		
+		pw.println("<br><a href='links.html'>home</a>");
+		
+		
+		pw.println("<br><b>Auth mode :: "+req.getAuthType()+"</b>");
+		pw.println("<br><b>logged in username :: "+req.getRemoteUser()+"</b>");
+		
+		
+		pw.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
